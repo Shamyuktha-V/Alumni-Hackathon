b1f1ea40a56deffd4fd72c9e34c580dfe8c1376d
ShamyukthaV <vshamyuktha@gmail.com>
2024-01-06 14:30:20 +0530

    level1a brutefroce

--- 21pt34_solution/level1.py
+++ 21pt34_solution/level1.py
@@ -48,32 +48,52 @@ vehicle_path=defaultdict(dict)
 
 vehicle_each_path=defaultdict(list)
 visited=[]
-capacity=0
 i=1
 while len(visited)<no_neighbours:
+    capacity=0
     min_dist_neigh=restaurant_neighbour_distance["r0"].index(min(restaurant_neighbour_distance["r0"]))
     min_dist_neigh_str="n"+str(min_dist_neigh)
     restaurant_neighbour_distance["r0"][min_dist_neigh]=float('inf')
     path_list=["r0"]
     capacity+=neighbours_quantity[min_dist_neigh_str]
+    while min_dist_neigh_str in visited:
+        min_dist_neigh=restaurant_neighbour_distance["r0"].index(min(restaurant_neighbour_distance["r0"]))
+        min_dist_neigh_str="n"+str(min_dist_neigh)
+        restaurant_neighbour_distance["r0"][min_dist_neigh]=float('inf')
     path_list.append(min_dist_neigh_str)
+    visited.append(min_dist_neigh_str)
     while capacity<=vehicles_capacity["v0"] and len(visited)<no_neighbours:
         neighbour=path_list[-1]
         neighbour_int=neighbour[1:]
         neighbours_distance[neighbour][int(neighbour_int)]=float('inf')
         min_dist_neigh=neighbours_distance[neighbour].index(min(neighbours_distance[neighbour]))
-        min_dist_neigh_str="n"+str(min_dist_neigh) 
-        print(min_dist_neigh_str)      
-        while min_dist_neigh_str in path_list and len(path_list)<no_neighbours+1 and capacity+neighbours_quantity[min_dist_neigh_str]>vehicles_capacity["v0"]:
-                neighbours_distance[neighbour][min_dist_neigh]=float('inf')
-                min_dist_neigh=neighbours_distance[neighbour].index(min(neighbours_distance[neighbour]))
-                min_dist_neigh_str="n"+str(min_dist_neigh)    
-        capacity+=neighbours_quantity[min_dist_neigh_str]
-        path_list.append(min_dist_neigh_str)
+        min_dist_neigh_str="n"+str(min_dist_neigh)
+        flag=1
+        while 1:     
+            while (min_dist_neigh_str in path_list and len(path_list)<no_neighbours+1) or capacity+neighbours_quantity[min_dist_neigh_str]>vehicles_capacity["v0"] or min_dist_neigh_str in visited:
+                    flag=1
+                    neighbours_distance[neighbour][min_dist_neigh]=float('inf')
+                    min_dist_neigh=neighbours_distance[neighbour].index(min(neighbours_distance[neighbour]))
+                    min_dist_neigh_str="n"+str(min_dist_neigh)
+                    min_dist=min(neighbours_distance[neighbour])
+                    if min_dist == float('inf'):
+                         flag=0
+                         break
+            if flag==0:
+                 break
+            if capacity+neighbours_quantity[min_dist_neigh_str]<=vehicles_capacity["v0"]:
+                capacity+=neighbours_quantity[min_dist_neigh_str]
+                path_list.append(min_dist_neigh_str)
+                visited.append(min_dist_neigh_str)
+                break
+        if flag==0:
+             break
     path_list.append("r0")
     path_no="path"+str(i)
     vehicle_each_path[path_no]=path_list
     i+=1
 
-print(vehicle_each_path)
+vehicle_path["v0"]=vehicle_each_path
+with open("21pt34_solution\\level1a_output.json","w") as outfile:
+    json.dump(vehicle_path,outfile)
 
